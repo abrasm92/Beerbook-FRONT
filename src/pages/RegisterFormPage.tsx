e111d28900a61c7c14208c5543e73acfb2d78f25
Abraham Saavedra <abrasm92@gmail.com>
2022-05-31 13:42:52 +0200

    added registerPage

---
+++ src/pages/RegisterFormPage.tsx
@@ -0,0 +1,7 @@
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const RegisterFormPage = (): JSX.Element => {
+  return <RegisterForm></RegisterForm>;
+};
+
+export default RegisterFormPage;
