7562cf551152b15f2cc43c892597ffe7d2eb574f
Abraham Saavedra <abrasm92@gmail.com>
2022-05-31 15:04:33 +0200

    added onchange to userRegister inputs

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,16 +1,50 @@
+import { useState } from "react";
 import RegisterFormStyles from "./RegisterFormStyles";
 
 const RegisterForm = (): JSX.Element => {
+  const initialFormValue = {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const [userData, setUserData] = useState(initialFormValue);
+
+  const changeUserData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [event.target.id]: event.target.value });
+  };
+
   return (
     <RegisterFormStyles noValidate autoComplete="off">
-      <label htmlFor="RegisterName">Nombre</label>
-      <input type="text" id="RegisterName" />
-      <label htmlFor="RegisterUsername">Usuario</label>
-      <input type="text" id="RegisterUsername" />
-      <label htmlFor="RegisterEmail">E-mail</label>
-      <input type="text" id="RegisterEmail" />
-      <label htmlFor="RegisterPassword">Contraseña</label>
-      <input type="text" id="RegisterPassword" />
+      <label htmlFor="name">Nombre</label>
+      <input
+        type="text"
+        id="name"
+        value={userData.name}
+        onChange={changeUserData}
+      />
+      <label htmlFor="username">Usuario</label>
+      <input
+        type="text"
+        id="username"
+        value={userData.username}
+        onChange={changeUserData}
+      />
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        id="email"
+        value={userData.email}
+        onChange={changeUserData}
+      />
+      <label htmlFor="password">Contraseña</label>
+      <input
+        type="password"
+        id="password"
+        value={userData.password}
+        onChange={changeUserData}
+      />
       <button>Registrarse</button>
       <p>Ya tienes cuenta? Inicia sesión aquí</p>
     </RegisterFormStyles>
