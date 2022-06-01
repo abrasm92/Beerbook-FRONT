8258b885c8d63b7405d27e16805829df968ce1c0
Abraham Saavedra <abrasm92@gmail.com>
2022-06-01 09:29:31 +0200

    handler to login

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,4 +7,7 @@ export const handlers = [
       return res(ctx.status(201), ctx.json("Usuario creado"));
     }
   ),
+  rest.post(`${process.env.REACT_APP_API_URL}user/login`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ token: "token" }));
+  }),
 ];
