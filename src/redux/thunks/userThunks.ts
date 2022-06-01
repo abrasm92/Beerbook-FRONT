6aa0c40b48af38d61fae7af9ff8c6927a6123c81
Abraham Saavedra <abrasm92@gmail.com>
2022-06-01 14:42:27 +0200

    dispatch login action

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosError } from "axios";
 import { Dispatch } from "redux";
-import { LoginUser, User } from "../../types/interfaces";
+import { LoginUser, User, UserResponseApi } from "../../types/interfaces";
 import { customErrorApi } from "../../utils/customerrorApi";
+import jwt_decode from "jwt-decode";
+import { userLoginActionCreator } from "../features/userSlice";
 
 export const userRegisterThunk = async (user: User) => {
   try {
@@ -24,12 +26,17 @@ export const userRegisterThunk = async (user: User) => {
 export const userLoginThunk =
   (user: LoginUser) => async (dispatch: Dispatch) => {
     try {
-      const { status } = await axios.post(
+      const { status, data } = await axios.post(
         `${process.env.REACT_APP_API_URL}user/login`,
         user
       );
       if (status === 200) {
         const message: string = "Has iniciado sessión";
+        const token = data.token;
+        const { username, id }: UserResponseApi = jwt_decode(token);
+        localStorage.setItem("token", token);
+        dispatch(userLoginActionCreator({ name: username, id }));
+        debugger;
         return message;
       }
     } catch (error: AxiosError | any) {
