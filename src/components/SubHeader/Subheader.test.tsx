8a7ca1753360fb63246e28b034dab9579ae65881
Abraham Saavedra <abrasm92@gmail.com>
2022-06-02 18:46:59 +0200

    test subheader

---
+++ src/components/SubHeader/Subheader.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../redux/store/store";
+import SubHeader from "./Subheader";
+
+describe("Given a SubHeader component", () => {
+  describe("When it's instantiated", () => {
+    test("Then it show it shoy a paragraph with a text", () => {
+      const expectedParagraph =
+        "Descubre todo sobre las mejores cervezas del mundo";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <SubHeader />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const text = screen.getByText(expectedParagraph);
+
+      expect(text).toBeInTheDocument();
+      expect(text).toHaveTextContent(expectedParagraph);
+    });
+  });
+});
--- src/components/SubHeader/Subheader.tsx
+++ src/components/SubHeader/Subheader.tsx
@@ -3,7 +3,7 @@ import SubHeaderStyles from "./SubHeaderStyles";
 const SubHeader = (): JSX.Element => {
   return (
     <SubHeaderStyles>
-      <p>Descubre todo sobre las mejores cervezas del mundo </p>
+      <p>Descubre todo sobre las mejores cervezas del mundo</p>
     </SubHeaderStyles>
   );
 };
