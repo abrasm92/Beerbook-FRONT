68b991170d0ac643113868550c4031a84050f7a4
Abraham Saavedra <abrasm92@gmail.com>
2022-06-02 22:10:49 +0200

    test button login

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -27,4 +27,29 @@ describe("Given a LoginForm component", () => {
       expect(inputPassword).toHaveAttribute("value", expectTextPassword);
     });
   });
+
+  describe("When it's rendered and user clicks buttom to submit the form", () => {
+    test("Then it shuold call usertRegisterThunk and reset inputs writeds", () => {
+      const expectTextUser = "";
+      const expectTextPassword = "";
+      const buttonName = "Iniciar sesión";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <LoginForm />
+          </Provider>
+        </BrowserRouter>
+      );
+      const inputUser = screen.getByLabelText("Usuario");
+      const inputPassword = screen.getByLabelText("Contraseña");
+      const button = screen.getByRole("button", { name: buttonName });
+      userEvent.type(inputUser, "brahams");
+      userEvent.type(inputPassword, "1234");
+      userEvent.click(button);
+
+      expect(inputUser).toHaveAttribute("value", expectTextUser);
+      expect(inputPassword).toHaveAttribute("value", expectTextPassword);
+    });
+  });
 });
