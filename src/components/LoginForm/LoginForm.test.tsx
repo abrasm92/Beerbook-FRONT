0c2b31de95ab9753a3677c7cd8f91084d5296c2b
Abraham Saavedra <abrasm92@gmail.com>
2022-06-02 13:04:52 +0200

    take off login button test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -27,30 +27,4 @@ describe("Given a LoginForm component", () => {
       expect(inputPassword).toHaveAttribute("value", expectTextPassword);
     });
   });
-
-  describe("When it's rendered and user clicks buttom to submit the form", () => {
-    test("Then it shuold call usertRegisterThunk and reset inputs writeds", () => {
-      jest.spyOn(Storage.prototype, "setItem").mockReturnThis();
-      const expectTextUser = "";
-      const expectTextPassword = "";
-      const buttonName = "Iniciar sesión";
-
-      render(
-        <BrowserRouter>
-          <Provider store={store}>
-            <LoginForm />
-          </Provider>
-        </BrowserRouter>
-      );
-      const inputUser = screen.getByLabelText("Usuario");
-      const inputPassword = screen.getByLabelText("Contraseña");
-      const button = screen.getByRole("button", { name: buttonName });
-      userEvent.type(inputUser, "brahams");
-      userEvent.type(inputPassword, "1234");
-      userEvent.click(button);
-
-      expect(inputUser).toHaveAttribute("value", expectTextUser);
-      expect(inputPassword).toHaveAttribute("value", expectTextPassword);
-    });
-  });
 });
