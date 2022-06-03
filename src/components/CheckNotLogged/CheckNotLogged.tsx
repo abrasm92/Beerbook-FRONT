02792b0c5a5cfbc223b2e4f9888a4a1cafa5bb47
Abraham Saavedra <abrasm92@gmail.com>
2022-06-03 17:48:27 +0200

    test checkNotLoged component

--- src/components/CheckNotLogged/CheckNotLogged.tsx
+++ src/components/CheckNotLogged/CheckNotLogged.tsx
@@ -8,6 +8,7 @@ type PropChildren = {
 
 const CheckNotLogged = ({ children }: PropChildren) => {
   const { logged } = useAppSelector((state) => state.user);
+  debugger;
   const navigate = useNavigate();
 
   useEffect(() => {
