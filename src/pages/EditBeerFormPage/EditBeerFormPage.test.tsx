4da53c2b97a23207315eedda4ccee9e337d5eed7
Abraham Saavedra <abrasm92@gmail.com>
2022-06-08 16:40:17 +0200

    test edit from page

--- src/pages/EditBeerFormPage/EditBeerFormPage.test.tsx
+++ src/pages/EditBeerFormPage/EditBeerFormPage.test.tsx
@@ -1,5 +1,10 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { render, screen } from "@testing-library/react";
+import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import reactTestRenderer from "react-test-renderer";
+import { groupOfBeer, singleBeer } from "../../mocks/beerMocks";
 import { store } from "../../redux/store/store";
 import EditBeerFormPage from "./EditBeerFormPage";
 
@@ -13,6 +18,41 @@ describe("Given a EditBeerFormPage component", () => {
       );
 
       expect(editForm).toMatchSnapshot();
+    });
+
+    test.only("Then it should call dispatch 1 time to get id by params", async () => {
+      const beerMockSlice = createSlice({
+        name: "beer",
+        initialState: {
+          page: 0,
+          listOfBeers: groupOfBeer,
+          singleBeer: singleBeer,
+        },
+        reducers: {},
+      });
+      const mockStore = configureStore({
+        reducer: { beer: beerMockSlice.reducer },
+      });
+      const mockId = `${singleBeer.id}`;
+      jest.mock("react-router-dom", () => ({
+        ...jest.requireActual("react-router-dom"),
+        useParams: () => ({
+          id: mockId,
+        }),
+      }));
+      const textButton = "Editar cerveza";
+
+      await render(
+        <BrowserRouter>
+          <Provider store={mockStore}>
+            <EditBeerFormPage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const buttonSendEdit = screen.getByRole("button", { name: textButton });
+
+      expect(buttonSendEdit).toBeInTheDocument();
     });
   });
 });
