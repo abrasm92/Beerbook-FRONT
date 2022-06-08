f45debea6c1294b7a96d4dd52e7280492b4ea8e8
Abraham Saavedra <abrasm92@gmail.com>
2022-06-08 15:29:49 +0200

    tests of beer slices

--- src/redux/features/beerSlice.test.ts
+++ src/redux/features/beerSlice.test.ts
@@ -3,7 +3,11 @@ import { BeerState } from "../../types/interfaces";
 import beerReducer, {
   createBeerActionCreator,
   deleteBeerActionCreator,
+  editBeerActionCreator,
+  getMaxPagesActionCreator,
   loadBeersActionCreator,
+  loadSingleBeerActionCreator,
+  setNumberPageActionCreator,
 } from "./beerSlice";
 
 describe("Given a beerSlice", () => {
@@ -117,6 +121,125 @@ describe("Given a beerSlice", () => {
       const recivedValue = beerReducer(initialState, action);
 
       expect(recivedValue.listOfBeers).toEqual(expectList);
+    });
+  });
+
+  describe("When it's invoked loadSingleBeerActionCreator", () => {
+    test("Then it should change state of singleBeer to same with a new valors of propierties as new load beer", () => {
+      const expectState = singleBeer;
+      const initialState: BeerState = {
+        listOfBeers: groupOfBeer,
+        page: 0,
+        totalPages: 0,
+        singleBeer: {
+          name: "",
+          brewery: "",
+          style: "",
+          degrees: 0,
+          IBU: NaN,
+          country: "",
+          description: "",
+          image: "",
+          owner: "",
+          id: "",
+        },
+      };
+
+      const action = loadSingleBeerActionCreator(singleBeer);
+
+      const recivedValue = beerReducer(initialState, action);
+
+      expect(recivedValue.singleBeer).toEqual(expectState);
+    });
+  });
+
+  describe("When it's invoked editBeerActionCreator", () => {
+    test("Then it should change state of listOfBeers from [] to array with list of beers and new beer", () => {
+      const updatedBeer = { ...singleBeer, name: "hola" };
+      const expectList = groupOfBeer.filter((beer) =>
+        beer.id === updatedBeer.id ? updatedBeer : beer
+      );
+      const initialState: BeerState = {
+        listOfBeers: groupOfBeer,
+        page: 0,
+        totalPages: 0,
+        singleBeer: {
+          name: "",
+          brewery: "",
+          style: "",
+          degrees: 0,
+          IBU: NaN,
+          country: "",
+          description: "",
+          image: "",
+          owner: "",
+          id: "",
+        },
+      };
+
+      const action = editBeerActionCreator(updatedBeer);
+
+      const recivedValue = beerReducer(initialState, action);
+
+      expect(recivedValue.listOfBeers).toEqual(expectList);
+    });
+  });
+
+  describe("When it's invoked getMaxPagesActionCreator", () => {
+    test("Then it should change state of totalPages to the new valor", () => {
+      const expectPages = 2;
+      const initialState: BeerState = {
+        listOfBeers: groupOfBeer,
+        page: 0,
+        totalPages: 0,
+        singleBeer: {
+          name: "",
+          brewery: "",
+          style: "",
+          degrees: 0,
+          IBU: NaN,
+          country: "",
+          description: "",
+          image: "",
+          owner: "",
+          id: "",
+        },
+      };
+
+      const action = getMaxPagesActionCreator(expectPages);
+
+      const recivedValue = beerReducer(initialState, action);
+
+      expect(recivedValue.totalPages).toEqual(expectPages);
+    });
+  });
+
+  describe("When it's invoked setNumberPageActionCreator", () => {
+    test("Then it should change state of page to the new valor", () => {
+      const expectPage = 2;
+      const initialState: BeerState = {
+        listOfBeers: groupOfBeer,
+        page: 0,
+        totalPages: 0,
+        singleBeer: {
+          name: "",
+          brewery: "",
+          style: "",
+          degrees: 0,
+          IBU: NaN,
+          country: "",
+          description: "",
+          image: "",
+          owner: "",
+          id: "",
+        },
+      };
+
+      const action = setNumberPageActionCreator(expectPage);
+
+      const recivedValue = beerReducer(initialState, action);
+
+      expect(recivedValue.page).toEqual(expectPage);
     });
   });
 });
