e7c84d9baed8ff3ac3e1f86c8f4baec5ce7ecee4
Abraham Saavedra <abrasm92@gmail.com>
2022-06-08 12:35:02 +0200

    reducer pagination

--- src/redux/features/beerSlice.ts
+++ src/redux/features/beerSlice.ts
@@ -55,6 +55,18 @@ const beerSlice = createSlice({
       ...beer,
       totalPages: action.payload,
     }),
+    upNumberPage: (beer): BeerState => ({
+      ...beer,
+      page: beer.page + 1 > beer.totalPages ? beer.page : beer.page + 1,
+    }),
+    downNumberPage: (beer): BeerState => ({
+      ...beer,
+      page: beer.page - 1 < 0 ? beer.page : beer.page - 1,
+    }),
+    setNumberPage: (beer, action: PayloadAction<number>): BeerState => ({
+      ...beer,
+      page: action.payload,
+    }),
   },
 });
 
